e3c162682ddc10378db5a09975251b165c435489
Kakit Wong <75346228+kitkatkandybar@users.noreply.github.com>
2022-04-11 10:18:01 -0400

    Update Truss_Class_Example.m

--- Truss_Class_Example.m
+++ Truss_Class_Example.m
@@ -45,15 +45,13 @@ Sy = [0, 1, 0;  %Joint 1
       0, 0, 0;  %Joint 7
       0, 0, 1]; %Joint 8
 
-    % Defines the X locations of each joint in m
+    % Location vectors in X and Y direction of each joint in m
 X = [0, 0, 4, 4, 8, 8, 12, 12];
 
-    % Defines the Y locations of each joint in m
 Y = [0, 4, 4, 8, 8, 4, 4, 0];
 
     % Load vector which represents the known forces acting on each joint
 L = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 0, 0, 0, 0];
-
 
 % Saving to a file
 save Truss_Class_Example.mat
